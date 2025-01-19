7ef1c036b3a591af35fa8ff6173aa6e2ca78cace
Crivion <crivion@gmail.com>
2025-01-19 13:07:31 +0200

    add vim blade navigation 'gf' support'

--- lua/plugins/vim-blade.lua
+++ lua/plugins/vim-blade.lua
@@ -1,3 +1,15 @@
 return {
-	"jwalton512/vim-blade",
+	{
+		"jwalton512/vim-blade",
+	},
+	{
+		"ricardoramirezr/blade-nav.nvim",
+		dependencies = { -- totally optional
+			"hrsh7th/nvim-cmp", -- if using nvim-cmp
+		},
+		ft = { "blade", "php" },
+		opts = {
+			close_tag_on_complete = true,
+		},
+	},
 }
