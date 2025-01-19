24a62e8b35af504592bc27431480aad1a872e8c2
Alex Crivion <crivion@gmail.com>
2025-01-19 10:17:58 +0200

    matchup for html tags

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -62,6 +62,10 @@ return { -- Highlight, edit, and navigate code
 				},
 			},
 		},
+		matchup = {
+			enable = true, -- mandatory, false will disable the whole extension
+			disable = {}, -- optional, list of language that will be disabled
+		},
 	},
 	-- There are additional nvim-treesitter modules that you can use to interact
 	-- with nvim-treesitter. You should go explore a few and see what interests you:
